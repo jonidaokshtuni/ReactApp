b73790f9db939ed91d1d5088c6b195e337bdfbe3
jonida.okshtuni <jonida.okshtuni@thinkopen.it>
2021-12-20 11:06:54 +0100

    using the useEffect Hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
@@ -7,14 +7,35 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     // console.log('CartContext', CartContext)
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         // console.log(curNumber, item.amount)
         return curNumber + item.amount
 
     }, 0);
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true)
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
     // console.log('numberOfCartItems', numberOfCartItems)
-    return <button className={classes.button} onClick={props.onClick}>
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
