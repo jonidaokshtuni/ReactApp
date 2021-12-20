2bd58155e73715f46dcdb9914f298add083cc1aa
jonida.okshtuni <jonida.okshtuni@thinkopen.it>
2021-12-20 10:44:56 +0100

    making items removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,14 +12,19 @@ const Cart = props => {
     const hasItems = cartCtx.items.length > 0;
 
     const cartItemRemoveHandler = id => {
-
+        cartCtx.removeItem(id)
     };
     const cartItemAddHandler = item => {
-
+        cartCtx.addItem(item);
     };
 
     const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map(item => (
-        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />
+        <CartItem key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)} />
     ))}</ul>
     return <Modal onClose={props.onClose}>
         {cartItems}
